7ecb635768ec80eee8de9f19714438c58e486cd8
GANGALIFE <nadaqver453@gmail.com>
2026-04-05 00:37:22 -0500

    Update env.js

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
 env.// Configuración centralizada para variables que deben venir de Vercel.
 // En Vercel agrega estos valores en Settings > Environment Variables.
 window.__ENV = window.__ENV || {};
-window.__ENV.DISCORD_WEBHOOK_URL = window.__ENV.DISCORD_WEBHOOK_URL || 'REEMPLAZA_CON_TU_WEBHOOK';
+window.__ENV.DISCORD_WEBHOOK_URL = window.__ENV.DISCORD_WEBHOOK_URL || 'https://discordapp.com/api/webhooks/1489904747845648515/mSnOtuwDSlQMJChVvLObPp22v8E5W6TQU49meRkvUuUw3dyiMHnptCmEdUQohYFULqMJ';
